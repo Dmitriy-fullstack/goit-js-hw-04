ab32e019a81dba1660ab6fa613e3285fbd952000
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-09-03 15:19:15 +0300

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,25 @@
-const countProps = function(obj) {
-   'use strict';
- // Write code under this line
- const keys = Object.keys(obj);
- return keys.length;
-};
-console.log(countProps({})); // 0
+const isUniq = (element, index, arr) => arr.indexOf(element) === index;
+const isEven = (element) => element % 2 === 0;
 
-console.log(countProps({a:1, b:1})); // 2
+function filterArray(array, cb) {
+  'use strict';
+  const numbers = [];
+  for(let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+    const result = cb(element, index, array);
+    if (result) {
+    numbers.push(element);}
+    // Write code under this line
+    
+  }
+  return numbers;
+}
 
-console.log(countProps({a:1, b:1, c:1, d:1, e:1})); // 5
+const arr  = [1,2,3,4,5,1,2,5];
+
+console.log(filterArray(arr, isUniq));
+// [1, 2, 3, 4, 5]
+
+console.log(filterArray(arr, isEven));
+// [2, 4, 2]
